a339a89a4178b1bba4d53dfc97560a9429b6a1b4
Sérgio Toledo <contato@sergiotoledo.com.br>
2023-06-30 17:01:20 -0300

    Body parts json files and basic translation feat

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import {
+  View,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import * as Speech from "expo-speech";
+
+const en = require("./data/en.json");
+const pt = require("./data/pt.json");
+const es = require("./data/es.json");
+
+// const dictionaries = {
+//   en: require("./data/en.json"),
+//   pt: require("./data/pt.json"),
+//   en: require("./data/en.json"),
+// };
 
 export default function App() {
+  useEffect(() => {
+    console.log(pt["body"]["arm"]);
+  }, []);
+
+  const speak = (text, language) => {
+    Speech.speak(text, {
+      // language: 'en-US',
+      // language: 'pt-BR',
+      // language: "es-ES",
+      language: language,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Body</Text>
+      <ScrollView>
+        {Object.keys(en["body"]).map((key) => (
+          <View style={styles.row} key={key}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => speak(pt["body"][key], "pt-BR")}
+            >
+              <Text style={styles.buttonText}>{pt["body"][key]}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => speak(en["body"][key], "en-US")}
+            >
+              <Text style={styles.buttonText}>{en["body"][key]}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => speak(es["body"][key], "es-ES")}
+            >
+              <Text style={styles.buttonText}>{es["body"][key]}</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +68,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 26,
+    color: "#c3c3c3",
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  row: {
+    backgroundColor: "#c3c3c3",
+    padding: 10,
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "#0f5389",
+    borderRadius: 5,
+    padding: 10,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
   },
 });
